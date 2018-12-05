d192c61d22c1788377252bf39c00c8eefbb16eb7
JkyProgrammer <32058792+JkyProgrammer@users.noreply.github.com>
2018-12-05 08:27:58 +0000

    Initial commit

---
+++ src/quadruplebranching/QuadrupleBranchingFractal.java
@@ -0,0 +1,34 @@
+package quadruplebranching;
+
+public class QuadrupleBranchingFractal {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public void write () {
+		
+	}
+	
+	public void baseGenerate () {
+		
+	}
+	
+	public void upGenerate () {
+		
+	}
+	
+	public void downGenerate () {
+		
+	}
+	
+	public void rightGenerate () {
+	
+	}
+	
+	public void leftGenerate () {
+		
+	}
+	
+	ArrayList<>
+}
