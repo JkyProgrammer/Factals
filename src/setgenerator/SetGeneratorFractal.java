79a8a7fe79771d3a7ddc18bc7375e3d176aaa367
JkyProgrammer <32058792+JkyProgrammer@users.noreply.github.com>
2018-12-06 17:22:18 +0000

    Did things

--- src/setgenerator/SetGeneratorFractal.java
+++ src/setgenerator/SetGeneratorFractal.java
@@ -22,29 +22,146 @@ public class SetGeneratorFractal {
 	}
 	
 	public static void main(String[] args) {
-		SetGeneratorFractal sgf = new SetGeneratorFractal ();
-		sgf.start(512, 256);
-	}
-	
-	public void start (int imageSize, int maxIterations) {
-		BufferedImage i = new BufferedImage (imageSize, imageSize, BufferedImage.TYPE_INT_ARGB);
-		for (int yLoc = 0; yLoc < imageSize; yLoc++) {
-			for (int xLoc = 0; xLoc < imageSize; xLoc++) {
-				int result = runSet (new Complex (xLoc, yLoc), maxIterations);
-				float mappedGrey = result/maxIterations;
-				Color col = new Color (mappedGrey, mappedGrey, mappedGrey);
-				int rgb = col.getRGB();
-				i.setRGB(xLoc, yLoc, rgb);
-			}
+		float xPos = 0.0f;
+		float yPos = 0.6f;
+		SetGeneratorFractal sgf = new SetGeneratorFractal (1024, 64, xPos, yPos, 0.1f);
+		sgf.prepare();
+		sgf.calculate();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 		
-		JFrame f = new JFrame ("Result");
+		for (float f = 0.5f; f < 3.0; f+= 0.5) {
+			System.out.println("Beginning analysis of zoom value " + f + ", at position: " + xPos + ", " + yPos);
+			sgf.setZoom(f);
+			sgf.calculate();
+		}
+		
+		System.out.println("Zooming finished.");
+	}
+	
+	public SetGeneratorFractal (int imageSize, int maxIterations, float xPos, float yPos, float zoom) {
+		this.imageSize = imageSize;
+		this.maxIterations = maxIterations;
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.zoom = zoom;
+	}
+	
+	private int imageSize;
+	private int maxIterations;
+	private float xPos;
+	private float yPos;
+	private float zoom;
+	
+	public void setZoom (float newZoom) {
+		zoom = newZoom;
+	}
+	
+	private BufferedImage i;
+	
+	private void processPixel (int xLoc, int yLoc) {
+		double xValue = (float)xLoc;
+		xValue -= imageSize/2;
+		
+		xValue /= ((float)imageSize);
+		xValue /= zoom;
+		
+		double yValue = (float)(yLoc);
+		yValue -= imageSize/2;
+		
+		yValue /= ((float)imageSize);
+		
+		yValue /= zoom;
+		yValue -= yPos;
+		
+		int result = runSet (new Complex (xValue, yValue), maxIterations);
+		float mappedGrey = 1f - (float)result/(float)maxIterations;
+		Color col = new Color (mappedGrey, mappedGrey, mappedGrey);
+		int rgb = col.getRGB();
+		i.setRGB(xLoc, yLoc, rgb);
+		
+		displayImage.getImage().flush();
+		f.repaint();
+	}
+	
+	private ImageIcon displayImage;
+	private JFrame f;
+	
+	public void prepare () {
+		i = new BufferedImage (imageSize, imageSize, BufferedImage.TYPE_INT_ARGB);
+		displayImage = new ImageIcon (i);
+		
+		f = new JFrame ("Result");
 		f.setSize(800, 800);
 		f.setLayout(new GridLayout ());
-		JLabel l = new JLabel(new ImageIcon (i));
+		JLabel l = new JLabel(displayImage);
+	
 		f.add(l);
 		
-		//f.setVisible(true);
+		f.setVisible(true);
+		f.pack();
+	}
+	
+	public void calculate () {
+		Thread t1 = new Thread (new Runnable () {
+			@Override
+			public void run() {
+				for (int yLoc = 0; yLoc < imageSize/4; yLoc++) {
+					for (int xLoc = 0; xLoc < imageSize; xLoc++) {
+						processPixel (xLoc, yLoc);
+					}
+				}
+			}
+		});
+		Thread t2 = new Thread (new Runnable () {
+			@Override
+			public void run() {
+				for (int yLoc = imageSize/4; yLoc < imageSize/2; yLoc++) {
+					for (int xLoc = 0; xLoc < imageSize; xLoc++) {
+						processPixel (xLoc, yLoc);
+					}
+				}
+			}
+		});
+		Thread t3 = new Thread (new Runnable () {
+			@Override
+			public void run() {
+				for (int yLoc = imageSize/2; yLoc < imageSize*3/4; yLoc++) {
+					for (int xLoc = 0; xLoc < imageSize; xLoc++) {
+						processPixel (xLoc, yLoc);
+					}
+				}
+			}
+		});
+		Thread t4 = new Thread (new Runnable () {
+			@Override
+			public void run() {
+				for (int yLoc = imageSize*3/4; yLoc < imageSize; yLoc++) {
+					for (int xLoc = 0; xLoc < imageSize; xLoc++) {
+						processPixel (xLoc, yLoc);
+					}
+				}
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		try {
+			t1.join();
+			t2.join();
+			t3.join();
+			t4.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println("Calculations done.");
 		
 		try {
 			File outputfile = new File("Mandelbrot.png");
