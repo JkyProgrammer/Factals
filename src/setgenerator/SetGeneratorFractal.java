967e694103d5fb4c3a72638ce67746676d78b19d
JkyProgrammer School <32058792+JkyProgSchool@users.noreply.github.com>
2018-12-06 13:46:36 +0000

    Worked on set generator

--- src/setgenerator/SetGeneratorFractal.java
+++ src/setgenerator/SetGeneratorFractal.java
@@ -1,29 +1,57 @@
 package setgenerator;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class SetGeneratorFractal {
 
-	public float runSet (Double c, int maxIterations) {
-		Double z = 0d;
+	public int runSet (Complex c, int maxIterations) {
+		Complex z = new Complex (0d,0d);
 		for (int its = 0; its < maxIterations; its++) {
-			if (Math.abs(z) > 2.0) return its;
-			z = (z*z) + c;
+			if (z.abs() > 2.0) return its;
+			z = z.times(z).plus(c);
 		}
 		return maxIterations;
 	}
 	
 	public static void main(String[] args) {
-		
+		SetGeneratorFractal sgf = new SetGeneratorFractal ();
+		sgf.start(512, 256);
 	}
-
-	JFrame frame = new JFrame ("SetGenerator");
 	
-	public void start () {
-		frame.setSize(400, 400);
+	public void start (int imageSize, int maxIterations) {
+		BufferedImage i = new BufferedImage (imageSize, imageSize, BufferedImage.TYPE_INT_ARGB);
+		for (int yLoc = 0; yLoc < imageSize; yLoc++) {
+			for (int xLoc = 0; xLoc < imageSize; xLoc++) {
+				int result = runSet (new Complex (xLoc, yLoc), maxIterations);
+				float mappedGrey = result/maxIterations;
+				Color col = new Color (mappedGrey, mappedGrey, mappedGrey);
+				int rgb = col.getRGB();
+				i.setRGB(xLoc, yLoc, rgb);
+			}
+		}
 		
-		BufferedImage i = new BufferedImage (4096, 4096, BufferedImage.TYPE_BYTE_GRAY);
-     
+		JFrame f = new JFrame ("Result");
+		f.setSize(800, 800);
+		f.setLayout(new GridLayout ());
+		JLabel l = new JLabel(new ImageIcon (i));
+		f.add(l);
+		
+		//f.setVisible(true);
+		
+		try {
+			File outputfile = new File("Mandelbrot.png");
+		    ImageIO.write(i, "png", outputfile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
