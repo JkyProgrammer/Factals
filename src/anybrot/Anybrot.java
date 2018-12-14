5db2042ed87c88e231bc660d010a69e2ac59651e
JkyProgrammer <32058792+JkyProgrammer@users.noreply.github.com>
2018-12-14 21:49:39 +0000

    Tidied

--- src/anybrot/Anybrot.java
+++ src/anybrot/Anybrot.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
@@ -179,107 +181,7 @@ public class Anybrot {
 		this.shouldBeVisual = shouldBeVisuall;
 		
 		if (shouldBeVisual) {
-			JFrame controlFrame = new JFrame ("Control Frame");
-			controlFrame.setLayout(new GridLayout (14, 1));
-			controlFrame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-			JLabel l1 = new JLabel ("Zoom Value");
-			tf1 = new JTextField ();
-			tf1.setText("" + zoom);
-			JLabel l2 = new JLabel ("Iteration Limit");
-			tf2 = new JTextField ();
-			tf2.setText("" + maxIterations);
-			JLabel l3 = new JLabel ("Position");
-			
-			xField = new JTextField ();
-			xField.setText("" + xPos);
-			
-			yField = new JTextField ();
-			yField.setText("" + yPos);
-			
-			JPanel pan = new JPanel ();
-			pan.setLayout(new GridLayout (1, 2));
-			pan.add(xField);
-			pan.add(yField);
-			
-			powerField = new JTextField ();
-			powerField.setText("" + power);
-			JLabel l4 = new JLabel ("Exponent Factor");
-			
-			palletSelector = new JComboBox<String> ();
-			
-			palletSelector.addItem("Greyscale");
-			palletSelector.addItem("Green to Yellow");
-			palletSelector.addItem("Blue to White");
-			
-			palletSelector.setSelectedIndex(selectedPallet);
-			
-			palletSelector.addActionListener(new ActionListener () {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					selectedPallet = palletSelector.getSelectedIndex();
-				}
-			});
-			JLabel l5 = new JLabel ("Colour Scheme");
-			
-			tf3 = new JTextField ();
-			tf3.setText ("" + this.colourGraduationDetail);
-			JLabel l6 = new JLabel ("Number of Colour Graduations");
-			
-			JButton b1 = new JButton ("Redraw image");
-			b1.addActionListener (new ActionListener () {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					try {
-						zoom = Float.parseFloat(tf1.getText());
-						maxIterations = Integer.parseInt(tf2.getText());
-						xPos = Float.parseFloat(xField.getText());
-						yPos = Float.parseFloat(yField.getText());
-						power = Float.parseFloat(powerField.getText());
-						colourGraduationDetail = Integer.parseInt(tf3.getText());
-						Thread t = new Thread (new Runnable () {
-							@Override
-							public void run() {
-								calculate ();
-							}
-						});
-						t.start();
-					} catch (Exception e1) {
-						e1.printStackTrace();
-					}
-				}
-			});
-			
-			JButton b2 = new JButton ("Save Image");
-			b2.addActionListener (new ActionListener () {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					save ("Image Latest");
-				}
-			});
-			
-			controlFrame.add(l1);
-			controlFrame.add(tf1);
-			
-			controlFrame.add(l2);
-			controlFrame.add(tf2);
-			
-			controlFrame.add(l3);
-			controlFrame.add(pan);
-			
-			controlFrame.add(l4);
-			controlFrame.add(powerField);
-			
-			controlFrame.add(l5);
-			controlFrame.add(palletSelector);
-			
-			controlFrame.add(l6);
-			controlFrame.add(tf3);
-			
-			controlFrame.add(b1);
-			controlFrame.add(b2);
-			
-			controlFrame.setSize(400, 300);
-			controlFrame.setVisible(true);
+			setupControlGUI ();
 		}
 	}
 	
@@ -362,6 +264,8 @@ public class Anybrot {
 		}
 	}
 	
+	private int selectedPallet = ColourPallet.blueToWhite;
+	private ArrayList<Color> colours;
 	private Color getColour (float initialMapping) {
 		float g = initialMapping * maxIterations;
 		if (initialMapping != 0.0) {
@@ -417,20 +321,6 @@ public class Anybrot {
 		
 	}
 	
-	private int selectedPallet = ColourPallet.blueToWhite;
-	private ImageIcon displayImage;
-	private JFrame f;
-	private JTextField tf1;
-	private JTextField tf2;
-	private JTextField xField;
-	private JTextField yField;
-	private JTextField powerField;
-	private JComboBox<String> palletSelector;
-	private JTextField tf3;
-	
-	private JComboBox<String> equationSelector;
-	private JTextField equationField;
-	
 	public void prepare () {
 		colours = new ArrayList<Color> ();
 		i = new BufferedImage (imageSize, imageSize, BufferedImage.TYPE_INT_ARGB);
@@ -469,7 +359,7 @@ public class Anybrot {
 		}
 	}
 	
-	private ArrayList<Color> colours;
+	
 	public void calculate () {
 		// Set up colours
 		colourSetup ();
@@ -544,5 +434,156 @@ public class Anybrot {
 			}
 		});
 		t.start();
+	}
+	
+	private ImageIcon displayImage;
+	private JFrame f;
+	private JTextField tf1;
+	private JTextField tf2;
+	private JTextField xField;
+	private JTextField yField;
+	private JTextField powerField;
+	private JComboBox<String> palletSelector;
+	private JTextField tf3;
+	
+	private JComboBox<String> equationSelector;
+	private JTextField equationField;
+	
+	private void setupControlGUI () {
+		JFrame controlFrame = new JFrame ("Control Frame");
+		controlFrame.setLayout(new GridLayout (14, 1));
+		controlFrame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		JLabel l1 = new JLabel ("Zoom Value");
+		tf1 = new JTextField ();
+		tf1.setText("" + zoom);
+		JLabel l2 = new JLabel ("Iteration Limit");
+		tf2 = new JTextField ();
+		tf2.setText("" + maxIterations);
+		JLabel l3 = new JLabel ("Position");
+		
+		xField = new JTextField ();
+		xField.setText("" + xPos);
+		
+		yField = new JTextField ();
+		yField.setText("" + yPos);
+		
+		JPanel pan = new JPanel ();
+		pan.setLayout(new GridLayout (1, 2));
+		pan.add(xField);
+		pan.add(yField);
+		
+		powerField = new JTextField ();
+		powerField.setText("" + power);
+		JLabel l4 = new JLabel ("Exponent Factor");
+		
+		palletSelector = new JComboBox<String> ();
+		
+		palletSelector.addItem("Greyscale");
+		palletSelector.addItem("Green to Yellow");
+		palletSelector.addItem("Blue to White");
+		
+		palletSelector.setSelectedIndex(selectedPallet);
+		
+		palletSelector.addActionListener(new ActionListener () {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				selectedPallet = palletSelector.getSelectedIndex();
+			}
+		});
+		JLabel l5 = new JLabel ("Colour Scheme");
+		
+		tf3 = new JTextField ();
+		tf3.setText ("" + this.colourGraduationDetail);
+		JLabel l6 = new JLabel ("Number of Colour Graduations");
+		
+		JLabel l7 = new JLabel ("Custom Equation Z = ");
+		equationField = new JTextField ("(Z^2) + C");
+		JPanel pan2 = new JPanel ();
+		pan.setLayout(new GridLayout (1, 2));
+		pan.add(l7);
+		pan.add(equationField);
+		
+		
+		equationSelector = new JComboBox<String> ();
+		equationSelector.addItem("Mandlebrot Set");
+		equationSelector.addItem("Multibrot Base");
+		equationSelector.addItem("Inverse Power Set");
+		equationSelector.addItem("Power Difference Base");
+		equationSelector.addItem("Custom");
+		
+		equationSelector.addItemListener(new ItemListener () {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					switch ((String)e.getItem()) {
+					// WIP
+					case "Mandelbrot Set":
+						equationField.setEnabled(false);
+						break;
+					}
+				}
+			}
+		});
+		
+		
+		JButton b1 = new JButton ("Redraw image");
+		b1.addActionListener (new ActionListener () {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					zoom = Float.parseFloat(tf1.getText());
+					maxIterations = Integer.parseInt(tf2.getText());
+					xPos = Float.parseFloat(xField.getText());
+					yPos = Float.parseFloat(yField.getText());
+					power = Float.parseFloat(powerField.getText());
+					colourGraduationDetail = Integer.parseInt(tf3.getText());
+					Thread t = new Thread (new Runnable () {
+						@Override
+						public void run() {
+							calculate ();
+						}
+					});
+					t.start();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		JButton b2 = new JButton ("Save Image");
+		b2.addActionListener (new ActionListener () {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				save ("Image Latest");
+			}
+		});
+		
+		
+		controlFrame.add(l1);
+		controlFrame.add(tf1);
+		
+		controlFrame.add(l2);
+		controlFrame.add(tf2);
+		
+		controlFrame.add(l3);
+		controlFrame.add(pan);
+		
+		controlFrame.add(l4);
+		controlFrame.add(powerField);
+		
+		controlFrame.add(l5);
+		controlFrame.add(palletSelector);
+		
+		controlFrame.add(l6);
+		controlFrame.add(tf3);
+		
+		controlFrame.add(equationSelector);
+		controlFrame.add(pan2);
+		
+		controlFrame.add(b1);
+		controlFrame.add(b2);
+		
+		controlFrame.setSize(400, 300);
+		controlFrame.setVisible(true);
 	}
 }
